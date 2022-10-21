43635df1a76aec204ab541236ed5e010d98527be
beki-d <bk14drobs@gmail.com>
2022-10-21 12:30:02 +0300

    Minor edit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building and designing exceptional websites. Currently, I’m focused on
+          I’m a computer engineer specializing in building and designing exceptional websites. Currently, I’m focused on
           building responsive full-stack web applications.
         </p>
         <div>
